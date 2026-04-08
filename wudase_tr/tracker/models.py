543cd21f49f7b6b9b3569eaa1235153bf0fbee50
Mastewal <mastewalgshiferaw@gmail.com>
2026-04-09 00:58:07 +0300

    added files in models to store uploaded images

--- wudase_tr/tracker/models.py
+++ wudase_tr/tracker/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Paragraph(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='wudase_paragraphs/')
+    day_name = models.CharField(max_length=20) # e.g., "Monday"
+    order_index = models.IntegerField() # 1, 2, 3...
+    is_mastered = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.day_name} - Part {self.order_index}"
+
+class DailyReview(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField(auto_now_add=True)
+    paragraphs_reviewed = models.ManyToManyField(Paragraph)
