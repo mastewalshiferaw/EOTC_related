b266dd78f615d8840165367ee52edf1216dc0ceb
mastewalshiferaw <mastewalgshiferaw@gmail.com>
2026-06-10 23:54:52 +0300

    Add authentication URLs to urlpatterns

--- wudase_tr/core/urls.py
+++ wudase_tr/core/urls.py
@@ -24,4 +24,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('tracker.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
