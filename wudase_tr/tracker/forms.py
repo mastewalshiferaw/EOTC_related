fca7e83a9259df600d48a6344f32fb1d50c59e7f
Mastewal <mastewalgshiferaw@gmail.com>
2026-06-11 15:01:13 +0300

    added a login and added a day name in forms

--- wudase_tr/tracker/forms.py
+++ wudase_tr/tracker/forms.py
@@ -6,7 +6,9 @@ class ParagraphForm(forms.ModelForm):
         model = Paragraph
         fields = ['day_name', 'order_index', 'image']
         widgets = {
-            'day_name': forms.Select(choices=[('Monday', 'ሰኞ'), ('Tuesday', 'ማክሰኞ'), ('Wednesday', 'ረቡዕ'), 
-                                              ('Thursday', 'ሐሙስ'), ('Friday', 'ዓርብ'), ('Saturday', 'ቅዳሜ'), ('Sunday', 'እሁድ')]),
+            'day_name': forms.Select(choices=[('Yezewetir Tselot', 'ጸሎተ ዘዘወትር'),('Monday', 'ሰኞ'), ('Tuesday', 'ማክሰኞ'), 
+                                              ('Wednesday', 'ረቡዕ'), 
+                                              ('Thursday', 'ሐሙስ'), ('Friday', 'ዓርብ'), ('Saturday', 'ቅዳሜ'), ('Sunday', 'እሁድ'), 
+                                              ('Anktse berhan', 'አንቀጽ ብርሀን'),('Melka Mariam', 'መልክዐ ማርያም' ),('Melka iyesus', 'መልክዐ እየሱስ')]),
             'order_index': forms.NumberInput(attrs={'placeholder': 'Paragraph Number'}),
         }
