e3035a3f645c7fe86a6faab03b45235cec5573c6
Mastewal <mastewalgshiferaw@gmail.com>
2026-06-20 23:43:59 +0300

    added a models

--- geez_translator_pro/translator/models.py
+++ geez_translator_pro/translator/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class GezDocument(models.Model):
+    title = models.CharField(max_length=255, blank=True)
+    file = models.FileField(upload_to='documents/')
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    
+    # This will hold the text we extract via OCR
+    extracted_geez_text = models.TextField(blank=True)
+    
+    # This will hold the final translation
+    translated_text = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"Doc {self.id} - {self.uploaded_at}"
